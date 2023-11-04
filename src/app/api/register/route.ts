bcf323975caf6abcc88458179281538400f76323
Harsh-0805 <harshbadala0805@gmail.com>
2023-11-04 14:05:25 +0530

    fix responsive

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,36 +1,59 @@
 import connectMongoDB from "../../../../libs/mongodb";
-import {User} from "../../../../models/user"; // Make sure to import the User model
+import { User } from "../../../../models/user"; // Make sure to import the User model
 import { NextResponse } from "next/server";
 
-export async function POST(request: { json: () => PromiseLike<{ teamName: any; password: any; profitLoss: any; description: any; totalMoney: any;imageLink:any ;isValid:any;}> | { teamName: any; password: any; profitLoss: any; description: any; totalMoney: any;imageLink:any; isValid:any;}; }) {
-    const {  teamName,
+export async function POST(request: {
+  json: () =>
+    | PromiseLike<{
+        teamName: any;
+        password: any;
+        profitLoss: any;
+        description: any;
+        totalMoney: any;
+        imageLink: any;
+        isValid: any;
+      }>
+    | {
+        teamName: any;
+        password: any;
+        profitLoss: any;
+        description: any;
+        totalMoney: any;
+        imageLink: any;
+        isValid: any;
+      };
+}) {
+  const {
+    teamName,
     password,
     profitLoss,
     description,
     totalMoney,
     imageLink,
-  isValid } = await request.json();
-  
-    try {
-      await connectMongoDB();
-      
-  
-      // Create a new user and save it to the database.
-      const newUser = new User({teamName,
-        password,
-        profitLoss,
-        description,
-        totalMoney,
-        imageLink ,
-        isValid});
-      await newUser.save();
-  
-      return NextResponse.json({ message: "User registered successfully" }, { status: 201 });
-    } catch (error) {
-      console.error("Error during user registration:", error);
-      return NextResponse.json({ message: "Server error" }, { status: 500 });
-    }
+    isValid,
+  } = await request.json();
+
+  try {
+    await connectMongoDB();
+
+    // Create a new user and save it to the database.
+    const newUser = new User({
+      teamName,
+      password,
+      profitLoss,
+      description,
+      totalMoney,
+      imageLink,
+      isValid,
+    });
+    await newUser.save();
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
-  
-  
-  
+}
