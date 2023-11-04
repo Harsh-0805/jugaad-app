d6981f0a012cd462e994f389befb4b317a818d03
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2023-11-05 00:32:26 +0530

    @/ added

--- src/app/leader/page.tsx
+++ src/app/leader/page.tsx
@@ -1,7 +1,7 @@
 // pages/index.tsx
 "use client";
 import React, { useEffect, useState } from "react";
-import "src/app/globals.css";
+import "@/app/globals.css";
 
 const IndexPage: React.FC = () => {
   const [leaderboardData, setLeaderboardData] = useState<any[]>([]);
